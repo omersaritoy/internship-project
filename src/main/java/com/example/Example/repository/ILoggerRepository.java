8e2eb130eadcc62bb6d93ab0e123e00db5e210d6
Ömer Sarıtoy <101520451+omersaritoy@users.noreply.github.com>
2024-07-25 14:33:09 +0300

    Gerekli sorgular eklendş

--- src/main/java/com/example/Example/repository/ILoggerRepository.java
+++ src/main/java/com/example/Example/repository/ILoggerRepository.java
@@ -1,10 +1,32 @@
 package com.example.Example.repository;
 
 import com.example.Example.entities.Logger;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import java.util.Optional;
 
-public interface ILoggerRepository extends JpaRepository<Logger,Long> {
-    Optional<Logger> findByEntityIdAndEntityType(Long entityId, String entityType);
+public interface ILoggerRepository extends JpaRepository<Logger, Long> {
+
+    Logger findByEntityIdAndEntityType(Long entityId, String entityType);
+
+    @Query("select l from Logger l")
+    List<Logger> getLogs();
+
+    @Query("SELECT l FROM Logger l WHERE l.id > :id")
+    List<Logger> findByIdGreaterThan(Long id);
+
+    @Query("select l from Logger l where l.entityType=:entityType")
+    List<Logger> findByEntityType(@Param("entityType") String entityType);
+
+    @Query("select l from Logger l where l.action=:action")
+    List<Logger> findByAction(@Param("action") String action);
+
+    @Query("SELECT l FROM Logger l WHERE (:entityType is null or l.entityType = :entityType) AND (:action IS NULL OR l.action = :action)")
+    List<Logger> findByEntityTypeAndAction(@Param("entityType") String entityType, @Param("action") String action);
+
 }

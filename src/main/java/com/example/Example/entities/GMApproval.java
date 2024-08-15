2619b622c1a50150fbc24b1143211de754b38394
Ömer Sarıtoy <101520451+omersaritoy@users.noreply.github.com>
2024-08-15 11:25:35 +0300

    porject updated

---
+++ src/main/java/com/example/Example/entities/GMApproval.java
@@ -0,0 +1,26 @@
+package com.example.Example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GMApproval {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+    @Transient
+    private Long employeeId;
+    private boolean approved;
+    private LocalDateTime requestTime;
+    private LocalDateTime approvalTime;
+}
